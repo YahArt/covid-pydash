afb209d49d788fd467bcf7e9f8868d79a60a7ebc
Yannick Hutter <yannick.hutter@solve.ch>
2022-06-07 17:45:29 +0200

    Add ability to directly navigate to created dashboard

--- frontend/src/app/components/create-dashboard/create-dashboard.component.ts
+++ frontend/src/app/components/create-dashboard/create-dashboard.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { Guid } from 'guid-typescript';
+import { AppRoutes } from 'src/app/config/app-routes';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { RouteHeadingService } from 'src/app/services/route-heading.service';
 
@@ -21,13 +22,13 @@ export class CreateDashboardComponent implements OnInit {
   );
 
 
-  constructor(private readonly dashboardService: DashboardService, private readonly snackbar: MatSnackBar, private readonly routeHeadingService: RouteHeadingService) { }
+  constructor(private readonly dashboardService: DashboardService, private readonly snackbar: MatSnackBar, private readonly routeHeadingService: RouteHeadingService, private readonly router: Router) { }
 
   public ngOnInit(): void {
     this.routeHeadingService.updateRouteHeadingTitle("Create a new Dashboard");
   }
 
-  public onCreateDashboard() {
+  public onCreateDashboard(editAfterCreation = false) {
     // TODO: Pass in correct template...
     const dashboard = this.dashboardService.createDashboardFromTemplate(null);
     dashboard.title = this.createDashboard.controls.name.value as string;
@@ -39,6 +40,12 @@ export class CreateDashboardComponent implements OnInit {
       this.snackbar.open(message, undefined, {
         duration: 1000
       });
+
+      if (editAfterCreation) {
+        this.router.navigate([AppRoutes.DASHBOARD], {
+          queryParams: { identifier: result.dashboard.identifier }
+        });
+      }
     })
   }
 
--- frontend/src/app/components/dashboard-overview/dashboard-overview.component.ts
+++ frontend/src/app/components/dashboard-overview/dashboard-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subject, takeUntil } from 'rxjs';
+import { finalize, Subject, takeUntil } from 'rxjs';
 import { AppRoutes } from 'src/app/config/app-routes';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { RouteHeadingService } from 'src/app/services/route-heading.service';
@@ -14,6 +14,10 @@ export class DashboardOverviewComponent implements OnInit, OnDestroy {
 
   private destroy = new Subject<void>();
 
+  public loading = false;
+
+  public hasDashboards = true;
+
   public dashboards: Array<{ identifier: string, title: string }> = [];
 
   public readonly displayedColumns: string[] = ['title', 'identifier', 'actions'];
@@ -22,8 +26,10 @@ export class DashboardOverviewComponent implements OnInit, OnDestroy {
   constructor(private readonly dashboardService: DashboardService, private readonly router: Router, private readonly routeHeadingService: RouteHeadingService) { }
 
   public ngOnInit(): void {
-    this.dashboardService.getDashboards$().pipe(takeUntil(this.destroy)).subscribe(response => {
+    this.loading = true;
+    this.dashboardService.getDashboards$().pipe(finalize(() => this.loading = false), takeUntil(this.destroy)).subscribe(response => {
       this.dashboards = response.dashboards;
+      this.hasDashboards = response.dashboards && response.dashboards.length > 0;
     });
     this.routeHeadingService.updateRouteHeadingTitle("Dashboard Overview");
   }
@@ -41,6 +47,11 @@ export class DashboardOverviewComponent implements OnInit, OnDestroy {
 
   public deleteDashboard(dashboard: { identifier: string, title: string }) {
     // TODO: Implement deleting a dashboard
+  }
+
+  public navigateToCreateDashboard($event: any): void {
+    $event.preventDefault();
+    this.router.navigate([AppRoutes.CREATE_DASHBOARD]);
   }
 
 }
